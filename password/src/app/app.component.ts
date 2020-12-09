aad58cd4b2a92a2d3689566cd84ac4697c07d421
Elvan H <elvanhd@gmail.com>
2020-12-09 23:49:58 +0700

    property binding syntax

--- password/src/app/app.component.ts
+++ password/src/app/app.component.ts
@@ -6,7 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  password: string = '';
+
   onButtonClick() {
-    console.log('Button was clicked');
+    this.password = 'MY PASSWORD!!!';
   }
 }
