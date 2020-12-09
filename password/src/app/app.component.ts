214f2730fc78fd2f6dddd1a1aa40183337dd5d34
Elvan H <elvanhd@gmail.com>
2020-12-10 00:21:59 +0700

    handling password length input

--- password/src/app/app.component.ts
+++ password/src/app/app.component.ts
@@ -7,27 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   password: string = '';
+  length: number = 8;
   includeLetters: boolean = true;
   includeNumbers: boolean = true;
   includeSymbols: boolean = true;
 
-  onChangeUseLetters() {
+  onChangeLength(value: number): void {
+    if (!isNaN(value)) {
+      this.length = value;
+    }
+  }
+
+  onChangeUseLetters(): void {
     this.includeLetters = !this.includeLetters;
   }
 
-  onChangeUseNumbers() {
+  onChangeUseNumbers(): void {
     this.includeNumbers = !this.includeNumbers;
   }
 
-  onChangeUseSymbols() {
+  onChangeUseSymbols(): void {
     this.includeSymbols = !this.includeSymbols;
   }
 
-  onButtonClick() {
+  onButtonClick(): void {
     this.password = 'MY PASSWORD!!!';
 
     console.log(`
     About to generate a password with the following:
+    Length: ${this.length}
     Use Letters: ${this.includeLetters}
     Use Numbers: ${this.includeNumbers}
     Use Symbols: ${this.includeSymbols}
