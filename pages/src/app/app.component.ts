f23fa68f796c08871e2cdc305f33093007592ae2
Elvan H <elvanhd@gmail.com>
2020-12-10 21:32:31 +0700

    getting some images urls

--- pages/src/app/app.component.ts
+++ pages/src/app/app.component.ts
@@ -3,8 +3,34 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pages';
+  images = [
+    {
+      title: 'coconut tree near shore within mountain range photo',
+      url:
+        'https://images.unsplash.com/photo-1519046904884-53103b34b206?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+      title: 'blue sea under blue sky and white clouds during daytime photo',
+      url:
+        'https://images.unsplash.com/photo-1585130401366-fe05a8d813c4?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NXx8YmVhY2h8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+      title: 'brown wooden walkway near beach during daytime photo',
+      url:
+        'https://images.unsplash.com/photo-1495954484750-af469f2f9be5?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NHx8YmVhY2h8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+      title: 'body of water near trees and mountain cliff during daytime photo',
+      url:
+        'https://images.unsplash.com/photo-1510414842594-a61c69b5ae57?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fGJlYWNofGVufDB8fDB8&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+      title: 'five birds flying on the sea photo',
+      url:
+        'https://images.unsplash.com/photo-1471922694854-ff1b63b20054?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTZ8fGJlYWNofGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    },
+  ];
 }
