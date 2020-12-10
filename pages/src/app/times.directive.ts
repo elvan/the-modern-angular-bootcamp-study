f4757d36ad47ede9727742ffe0b8c2dcf4b22503
Elvan H <elvanhd@gmail.com>
2020-12-11 00:12:41 +0700

    context in structural directives

--- pages/src/app/times.directive.ts
+++ pages/src/app/times.directive.ts
@@ -13,7 +13,9 @@ export class TimesDirective {
     this.viewContainerRef.clear();
 
     for (let i = 0; i < times; i++) {
-      this.viewContainerRef.createEmbeddedView(this.templateRef);
+      this.viewContainerRef.createEmbeddedView(this.templateRef, {
+        index: i,
+      });
     }
   }
 }
