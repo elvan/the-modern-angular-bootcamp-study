7b2e4618046b70d6151cd717baf2e1ec65a5b141
Elvan H <elvanhd@gmail.com>
2020-12-10 23:29:29 +0700

    communicating properties to directive

--- pages/src/app/class.directive.ts
+++ pages/src/app/class.directive.ts
@@ -1,10 +1,14 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appClass]',
 })
 export class ClassDirective {
+  @Input() backgroundColor = '';
   constructor(private elementRef: ElementRef) {
-    this.elementRef.nativeElement.style.backgroundColor = 'orange';
+    // NEVER DO THIS!!!
+    setTimeout(() => {
+      this.elementRef.nativeElement.style.backgroundColor = this.backgroundColor;
+    }, 100);
   }
 }
