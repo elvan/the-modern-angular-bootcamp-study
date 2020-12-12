b79cc74a1e235e9a5f9fd16b4006577d94991e5a
Elvan H <elvanhd@gmail.com>
2020-12-12 14:34:50 +0700

    adding child navigation routes

--- components/src/app/collections/collections.module.ts
+++ components/src/app/collections/collections.module.ts
@@ -1,12 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
+import { BiographyComponent } from './biography/biography.component';
 import { CollectionsHomeComponent } from './collections-home/collections-home.component';
 import { CollectionsRoutingModule } from './collections-routing.module';
+import { CompaniesComponent } from './companies/companies.component';
+import { PartnersComponent } from './partners/partners.component';
 import { TableComponent } from './table/table.component';
 
 @NgModule({
-  declarations: [CollectionsHomeComponent, TableComponent],
+  declarations: [
+    CollectionsHomeComponent,
+    TableComponent,
+    BiographyComponent,
+    CompaniesComponent,
+    PartnersComponent,
+  ],
   imports: [CommonModule, CollectionsRoutingModule, SharedModule],
 })
 export class CollectionsModule {}
