5587338c4b4f43263a684857f12e2d8a190c399a
Elvan H <elvanhd@gmail.com>
2020-12-13 07:36:23 +0700

    listing accordion elements

--- components/src/app/mods/accordion/accordion.component.ts
+++ components/src/app/mods/accordion/accordion.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-accordion',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./accordion.component.css'],
 })
 export class AccordionComponent implements OnInit {
+  @Input() items = [];
+
   constructor() {}
 
   ngOnInit(): void {}
