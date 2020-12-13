6be8833fcf3888880e412e1cdc3ef5131de55531
Elvan H <elvanhd@gmail.com>
2020-12-13 17:01:32 +0700

    interfaces in typescript

--- typescript-overview/152-difficulties-with-objects.ts
+++ typescript-overview/152-difficulties-with-objects.ts
@@ -1,13 +1,17 @@
-const post = {
+interface Post {
+  title: string;
+  daysOld: number;
+  published: boolean;
+}
+
+const post: Post = {
   title: 'Latest TypeScript News',
   daysOld: 10,
   published: true,
 };
 
-const printPost = (postToPrint: {
-  title: string;
-  daysOld: number;
-  published: boolean;
-}) => {
+const printPost = (postToPrint: Post) => {
   return `${postToPrint.title} (${postToPrint.daysOld} days old)`;
 };
+
+printPost(post);
