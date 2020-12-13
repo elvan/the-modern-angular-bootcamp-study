640a1a07053f0b96585d805d4a88d086bd6ec8ec
Elvan H <elvanhd@gmail.com>
2020-12-13 17:12:01 +0700

    classes and properties

---
+++ typescript-overview/154-classes-and-properties.ts
@@ -0,0 +1,18 @@
+class Car {
+  color: string;
+  year: number;
+
+  constructor(color: string, year: number) {
+    this.color = color;
+    this.year = year;
+  }
+
+  drive() {
+    console.log('Vroom');
+  }
+}
+
+const myCar = new Car('red', 2000);
+myCar.drive();
+
+console.log(myCar.color, myCar.year);
