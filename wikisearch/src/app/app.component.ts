46ec37817c40beff413391894dc4e3081f935edc
Elvan H <elvanhd@gmail.com>
2020-12-14 20:57:33 +0700

    app-component to page-list communication

--- wikisearch/src/app/app.component.ts
+++ wikisearch/src/app/app.component.ts
@@ -7,11 +7,13 @@ import { WikipediaService } from './wikipedia.service';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  pages = [];
+
   constructor(private wikipediaService: WikipediaService) {}
 
   onTermSubmitted(term: string) {
-    this.wikipediaService.search(term).subscribe((response) => {
-      console.log(response);
+    this.wikipediaService.search(term).subscribe((response: any) => {
+      this.pages = response.query.search;
     });
   }
 }
