e807c55542935a89e0f88caaa775897ea1c4cb7a
Elvan H <elvanhd@gmail.com>
2020-12-14 09:18:52 +0700

    adding an input to the search-bar

--- wikisearch/src/app/search-bar/search-bar.component.ts
+++ wikisearch/src/app/search-bar/search-bar.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-bar.component.css'],
 })
 export class SearchBarComponent implements OnInit {
+  term = '';
+
   constructor() {}
 
   ngOnInit(): void {}
