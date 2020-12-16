d28a0cebdda050f8c9a9a9d0eb84e246c6f7a6b6
Elvan H <elvanhd@gmail.com>
2020-12-16 19:53:08 +0700

    adding an svg credit card image

--- credit-card/src/app/app.module.ts
+++ credit-card/src/app/app.module.ts
@@ -5,11 +5,12 @@ import { IConfig, NgxMaskModule } from 'ngx-mask';
 import { AppComponent } from './app.component';
 import { CardFormComponent } from './card-form/card-form.component';
 import { InputComponent } from './input/input.component';
+import { CardComponent } from './card/card.component';
 
 export const options: Partial<IConfig> | (() => Partial<IConfig>) = {};
 
 @NgModule({
-  declarations: [AppComponent, CardFormComponent, InputComponent],
+  declarations: [AppComponent, CardFormComponent, InputComponent, CardComponent],
   imports: [BrowserModule, ReactiveFormsModule, NgxMaskModule.forRoot(options)],
   providers: [],
   bootstrap: [AppComponent],
