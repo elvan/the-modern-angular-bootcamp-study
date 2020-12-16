8fa060537741beec1935814c5ee30e1863f4b2af
Elvan H <elvanhd@gmail.com>
2020-12-16 14:29:46 +0700

    additional validations

--- credit-card/src/app/card-form/card-form.component.ts
+++ credit-card/src/app/card-form/card-form.component.ts
@@ -13,9 +13,22 @@ export class CardFormComponent implements OnInit {
       Validators.minLength(3),
       Validators.maxLength(50),
     ]),
-    cardNumber: new FormControl(''),
-    expiration: new FormControl(''),
-    securityCode: new FormControl(''),
+    cardNumber: new FormControl('', [
+      Validators.required,
+      Validators.minLength(16),
+      Validators.maxLength(16),
+    ]),
+    expiration: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^(0[1-9]|1[1-2])\/\d{2}$/),
+    ]),
+    securityCode: new FormControl('', [
+      Validators.required,
+      Validators.min(100),
+      Validators.max(999),
+      Validators.minLength(3),
+      Validators.maxLength(3),
+    ]),
   });
 
   constructor() {}
