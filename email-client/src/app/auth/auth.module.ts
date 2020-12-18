0c8511fcb8cbb7250c2aaca4d3f18916b390cea8
Elvan H <elvanhd@gmail.com>
2020-12-19 06:45:25 +0700

    creating a reusable input component

--- email-client/src/app/auth/auth.module.ts
+++ email-client/src/app/auth/auth.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [SignInComponent, SignUpComponent],
-  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule],
+  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule, SharedModule],
 })
 export class AuthModule {}
--- email-client/src/app/shared/shared.module.ts
+++ email-client/src/app/shared/shared.module.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { InputComponent } from './input/input.component';
 
 @NgModule({
   declarations: [InputComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
+  exports: [InputComponent],
 })
 export class SharedModule {}
