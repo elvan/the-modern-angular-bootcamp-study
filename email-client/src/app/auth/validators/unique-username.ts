ca5223d2db4f2ddd0787b2b3b08bd1159d3d02df
Elvan H <elvanhd@gmail.com>
2020-12-19 01:09:07 +0700

    building an auth service

--- email-client/src/app/auth/validators/unique-username.ts
+++ email-client/src/app/auth/validators/unique-username.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import {
   AbstractControl,
@@ -7,28 +6,29 @@ import {
 } from '@angular/forms';
 import { Observable, of } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
+import { AuthService } from '../auth.service';
 
 @Injectable({ providedIn: 'root' })
 export class UniqueUsername implements AsyncValidator {
-  constructor(private httpClient: HttpClient) {}
+  constructor(private authService: AuthService) {}
 
   validate = (
     control: AbstractControl
   ): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> => {
     const { value } = control;
 
-    return this.httpClient
-      .post('https://api.angular-email.com/auth/username', { username: value })
-      .pipe(
-        map((value) => {
+    return this.authService.usernameAvailable(value).pipe(
+      map((value) => {
+        if (value.available) {
           return null;
-        }),
-        catchError((err) => {
-          if (err.error.username) {
-            return of({ nonUniqueUsername: true });
-          }
-          return of({ noConnection: true });
-        })
-      );
+        }
+      }),
+      catchError((err) => {
+        if (err.error.username) {
+          return of({ nonUniqueUsername: true });
+        }
+        return of({ noConnection: true });
+      })
+    );
   };
 }
