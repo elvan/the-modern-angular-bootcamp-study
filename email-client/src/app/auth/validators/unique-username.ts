f40fa01e916e8ca1a35c99341d5728fc4c238d24
Elvan H <elvanhd@gmail.com>
2020-12-18 21:59:17 +0700

    returns an observable from the async validator

--- email-client/src/app/auth/validators/unique-username.ts
+++ email-client/src/app/auth/validators/unique-username.ts
@@ -1,19 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {
-  AbstractControl,
-  AsyncValidator,
-  ValidationErrors,
-} from '@angular/forms';
-import { Observable } from 'rxjs';
+import { AsyncValidator, FormControl, ValidationErrors } from '@angular/forms';
+import { Observable, of } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class UniqueUsername implements AsyncValidator {
   constructor(private httpClient: HttpClient) {}
 
-  validate(
-    control: AbstractControl
-  ): Promise<ValidationErrors> | Observable<ValidationErrors> {
-    return null;
-  }
+  validate = (
+    control: FormControl
+  ): Promise<ValidationErrors> | Observable<ValidationErrors> => {
+    const { value } = control;
+    console.log(this.httpClient);
+    return of({ uniqueUsername: true });
+  };
 }
