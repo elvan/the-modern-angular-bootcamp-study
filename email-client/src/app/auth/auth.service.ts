5c235f26b31b7ed85ba2b0a3fdc18ac3673c7432
Elvan H <elvanhd@gmail.com>
2020-12-19 11:38:02 +0700

    refactoring auth service

--- email-client/src/app/auth/auth.service.ts
+++ email-client/src/app/auth/auth.service.ts
@@ -1,33 +1,42 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-interface UsernameAvailableResponse {
+interface UsernameResponse {
   available: boolean;
+}
+
+interface SignUpCredentials {
+  username: string;
+  password: string;
+  passwordConfirmation: string;
+}
+
+interface SignUpResponse {
+  username: string;
 }
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  rootUrl = 'https://api.angular-email.com';
+
   constructor(private httpClient: HttpClient) {}
 
   usernameAvailable(username: string) {
-    return this.httpClient.post<UsernameAvailableResponse>(
-      'https://api.angular-email.com/auth/username',
+    return this.httpClient.post<UsernameResponse>(
+      `${this.rootUrl}/auth/username`,
       {
         username: username,
       }
     );
   }
 
-  signUp(credentials: any) {
-    return this.httpClient.post<any>(
-      'https://api.angular-email.com/auth/signup',
-      {
-        username: credentials.username,
-        password: credentials.password,
-        passwordConfirmation: credentials.passwordConfirmation,
-      }
-    );
+  signUp(credentials: SignUpCredentials) {
+    return this.httpClient.post<SignUpResponse>(`${this.rootUrl}/auth/signup`, {
+      username: credentials.username,
+      password: credentials.password,
+      passwordConfirmation: credentials.passwordConfirmation,
+    });
   }
 }
