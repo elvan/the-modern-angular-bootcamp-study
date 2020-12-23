75d6f57aad7d634be0f6482b67e4564f7c44859d
Elvan H <elvanhd@gmail.com>
2020-12-23 23:37:12 +0700

    filter only loggin request that was sent to server

--- email-client/src/app/auth/auth-http-interceptor.ts
+++ email-client/src/app/auth/auth-http-interceptor.ts
@@ -1,11 +1,13 @@
 import {
   HttpEvent,
+  HttpEventType,
   HttpHandler,
   HttpInterceptor,
   HttpRequest,
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { filter, tap } from 'rxjs/operators';
 
 @Injectable()
 export class AuthHttpInterceptor implements HttpInterceptor {
@@ -17,6 +19,11 @@ export class AuthHttpInterceptor implements HttpInterceptor {
       withCredentials: true,
     });
 
-    return next.handle(modifiedReq);
+    return next.handle(modifiedReq).pipe(
+      filter((value) => value.type === HttpEventType.Sent),
+      tap((value) => {
+        console.log('Request was sent to server');
+      })
+    );
   }
 }
