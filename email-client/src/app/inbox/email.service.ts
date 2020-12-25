6e6c585a3cbf3de78d93ea733745a3fedd63a323
Elvan H <elvanhd@gmail.com>
2020-12-25 13:32:46 +0700

    adding email fetching to the service

--- email-client/src/app/inbox/email.service.ts
+++ email-client/src/app/inbox/email.service.ts
@@ -1,8 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+interface EmailSummary {
+  id: string;
+  subject: string;
+  from: string;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class EmailService {
-  constructor() {}
+  rootUrl = 'https://api.angular-email.com';
+
+  constructor(private httpClient: HttpClient) {}
+
+  getEmails() {
+    return this.httpClient.get<EmailSummary[]>(`${this.rootUrl}/emails`);
+  }
 }
