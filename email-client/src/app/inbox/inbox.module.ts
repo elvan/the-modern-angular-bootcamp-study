478d1f177f588c894f0b8ae85b241a0b01dc3478
Elvan H <elvanhd@gmail.com>
2020-12-25 10:04:07 +0700

    generating inbox components

--- email-client/src/app/inbox/inbox.module.ts
+++ email-client/src/app/inbox/inbox.module.ts
@@ -1,10 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { EmailCreateComponent } from './email-create/email-create.component';
+import { EmailIndexComponent } from './email-index/email-index.component';
+import { EmailReplyComponent } from './email-reply/email-reply.component';
+import { EmailShowComponent } from './email-show/email-show.component';
 import { HomeComponent } from './home/home.component';
 import { InboxRoutingModule } from './inbox-routing.module';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [
+    HomeComponent,
+    EmailCreateComponent,
+    EmailReplyComponent,
+    EmailIndexComponent,
+    EmailShowComponent,
+  ],
   imports: [CommonModule, InboxRoutingModule],
 })
 export class InboxModule {}
