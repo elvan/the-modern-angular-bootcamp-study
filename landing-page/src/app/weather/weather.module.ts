ab22e959bb3aa89878f9afcd7cc9134c784b871d
Elvan H <elvanhd@gmail.com>
2020-12-28 09:54:15 +0700

    connecting modules

--- landing-page/src/app/weather/weather.module.ts
+++ landing-page/src/app/weather/weather.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { ForecastComponent } from './forecast/forecast.component';
 
 @NgModule({
   declarations: [ForecastComponent],
   imports: [CommonModule],
+  exports: [ForecastComponent],
 })
 export class WeatherModule {}
