e394fab3d47167223df5885bb32a46d88b517931
Elvan H <elvanhd@gmail.com>
2020-12-29 19:13:51 +0700

    generating the notifications module

--- landing-page/src/app/app.module.ts
+++ landing-page/src/app/app.module.ts
@@ -2,11 +2,17 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { NotificationsModule } from './notifications/notifications.module';
 import { WeatherModule } from './weather/weather.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, WeatherModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    WeatherModule,
+    NotificationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
